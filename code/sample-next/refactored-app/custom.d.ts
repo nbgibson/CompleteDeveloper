0a711f830426783d26044c1988dfd690b311a3f8
Nate Gibson <natebgibson@gmail.com>
2024-03-09 21:31:18 -0600

    Finishing Next.js section

---
+++ code/sample-next/refactored-app/custom.d.ts
@@ -0,0 +1,7 @@
+interface WeatherProps {
+  weather: string;
+}
+
+type WeatherDetailType = { zipcode: string; weather: string; temp?: number };
+
+type responseItemType = { id: string; name: string };
