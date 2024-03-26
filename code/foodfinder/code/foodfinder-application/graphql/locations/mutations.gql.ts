c674ce3376fb0f217104f4f73124e1c2f14ffa1c
Nate Gibson <natebgibson@gmail.com>
2024-03-25 21:24:19 -0400

    Wrapping up graphql implementation

---
+++ code/foodfinder/code/foodfinder-application/graphql/locations/mutations.gql.ts
@@ -0,0 +1,3 @@
+export default ` addWishlist(location_id: String!, user_id: String!): Location!
+removeWishlist(location_id: String!, user_id: String!): Location!
+`;
---
+++ code/foodfinder/code/foodfinder-application/graphql/locations/queries.gql.ts
@@ -0,0 +1,4 @@
+export default ` allLocations: [Location]!
+locationsById(location_ids: [String]!): [Location]!
+onUserWishlist(user_id: String!): [Location]!
+`;
