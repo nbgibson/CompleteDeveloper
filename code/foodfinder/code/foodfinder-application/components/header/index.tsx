61e786a4c3c2b3e88e0fa856d54a18ebfb424fd1
Nate Gibson <natebgibson@gmail.com>
2024-03-26 22:22:44 -0400

    Wrapping up frontend work

---
+++ code/foodfinder/code/foodfinder-application/components/header/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./index.module.css";
+import Logo from "components/header/logo";
+
+const Header = (): JSX.Element => {
+  return (
+    <header className={styles.root}>
+      {" "}
+      <div className="layout-grid">
+        {" "}
+        <Logo />{" "}
+      </div>{" "}
+    </header>
+  );
+};
+
+export default Header;
